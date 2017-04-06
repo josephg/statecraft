de4b17473849977b09dcc65f8db2dd8680df67d9
Joseph Gentle <me@josephg.com>
2017-04-06 11:15:57 +1000

    Moved subutil cursorAll into easyapi

--- test/lens.js
+++ test/lens.js
@@ -2,66 +2,66 @@ const lens = require('../lib/lens')
 const assert = require('assert')
 const {assertMapEq} = require('./util')
 
-describe('lens', function() {
-  beforeEach(function() {
-    // Just using a stub. TODO: Replace me with a memory store or something.
-    this.store = {
-      supportedQueryTypes: {kv:true, sortedkv:true},
-    }
-
-    this.lens = lens(this.store, 'x/')
-  })
-
-  describe('fetch', () => {
-    it('correctly scopes in kv queries', function(done) {
-      const opts = {}
-      this.store.fetch = (qtype, query, options, callback) => {
-        assert.strictEqual(qtype, 'kv')
-        assert.deepEqual(query, ['x/a', 'x/b']) // TODO: Also ok to get a set here.
-        assert.strictEqual(options, opts)
-
-        callback(null, {
-          results: new Map([['x/a', 5], ['x/b', 7]]),
-          versions: {}
-        })
-
+describe('lens', () => {
+  describe('lens specific tests', () => {
+    beforeEach(function() {
+      // Just using a stub. TODO: Replace me with a memory store or something.
+      this.store = {
+        supportedQueryTypes: {kv:true, sortedkv:true},
       }
 
-      this.lens.fetch('kv', ['a', 'b'], opts, (err, {results, versions}) => {
-        if (err) throw err
-        assert.deepEqual(results, new Map([['a', 5], ['b', 7]]))
-        done()
-      })
+      this.lens = lens(this.store, 'x/')
     })
 
-    it('correctly scopes in skv queries', function(done) {
-      const opts = {}
-      this.store.fetch = (qtype, query, options, callback) => {
-        assert.strictEqual(qtype, 'sortedkv')
-        assert.deepEqual(query, ['<x/a', 1, '.', 0, '>x/b', 1, '>x/c'])
-        assert.strictEqual(options, opts)
+    describe('fetch', () => {
+      it('correctly scopes in kv queries', function(done) {
+        const opts = {}
+        this.store.fetch = (qtype, query, options, callback) => {
+          assert.strictEqual(qtype, 'kv')
+          assert.deepEqual(query, ['x/a', 'x/b']) // TODO: Also ok to get a set here.
+          assert.strictEqual(options, opts)
 
-        callback(null, {
-          results: new Map([['x/a', 5], ['x/c', 7]]),
-          versions: {}
+          callback(null, {
+            results: new Map([['x/a', 5], ['x/b', 7]]),
+            versions: {}
+          })
+
+        }
+
+        this.lens.fetch('kv', ['a', 'b'], opts, (err, {results, versions}) => {
+          if (err) throw err
+          assert.deepEqual(results, new Map([['a', 5], ['b', 7]]))
+          done()
         })
-
-      }
-
-      this.lens.fetch('sortedkv', ['<a', 1, '.', 0, '>b', 1, '>c'], opts, (err, {results, versions}) => {
-        if (err) throw err
-        assert.deepEqual(results, new Map([['a', 5], ['c', 7]]))
-        done()
       })
+
+      it('correctly scopes in skv queries', function(done) {
+        const opts = {}
+        this.store.fetch = (qtype, query, options, callback) => {
+          assert.strictEqual(qtype, 'sortedkv')
+          assert.deepEqual(query, ['<x/a', 1, '.', 0, '>x/b', 1, '>x/c'])
+          assert.strictEqual(options, opts)
+
+          callback(null, {
+            results: new Map([['x/a', 5], ['x/c', 7]]),
+            versions: {}
+          })
+
+        }
+
+        this.lens.fetch('sortedkv', ['<a', 1, '.', 0, '>b', 1, '>c'], opts, (err, {results, versions}) => {
+          if (err) throw err
+          assert.deepEqual(results, new Map([['a', 5], ['c', 7]]))
+          done()
+        })
+      })
+
+      it('errors when encountering another kind of query')
+      it('errors if the database returns results outside of the expected range') // TODO: Or should it discard outside values?
     })
 
-    it('errors when encountering another kind of query')
-    it('errors if the database returns results outside of the expected range') // TODO: Or should it discard outside values?
-  })
-
-  describe('subscribe', () => {
-
-
+    describe('subscribe', () => {
+    })
   })
 
   {
