f3260621792e06f39afde090e00a44a98b1019cf
Joseph Gentle <me@josephg.com>
2018-03-20 11:44:35 +1100

    Added some missing tests

---
+++ test/net.ts
@@ -0,0 +1,27 @@
+import 'mocha'
+import kvStore from '../lib/stores/kvmem'
+import augment from '../lib/augment'
+import createServer from '../lib/tcpserver'
+import connectStore from '../lib/stores/remote'
+import runTests from './common'
+import * as I from '../lib/types/interfaces'
+
+describe('remote', () => {
+  const serverForStore = new WeakMap<I.Store, any>()
+  runTests(() => new Promise((resolve, reject) => {
+    const store = augment(kvStore())
+    const server = createServer(store)
+    server.listen(0, () => {
+      const port = server.address().port
+      connectStore(port, 'localhost', (err, remoteStore) => {
+        if (err) {
+          server.close()
+          return reject(err)
+        } else {
+          serverForStore.set(remoteStore!, server)
+          resolve(remoteStore!)
+        }
+      })
+    })
+  }), (store) => serverForStore.get(store).close())
+})
