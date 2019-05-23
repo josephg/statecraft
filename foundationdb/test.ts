aed7d182c6f9b1af8bf1094f89ed87d0f58fec22
Joseph Gentle <me@josephg.com>
2019-05-23 14:07:46 +1000

    Fixed foundationdb tests

--- foundationdb/test.ts
+++ foundationdb/test.ts
@@ -1,47 +1,49 @@
-// import 'mocha'
-// import * as I from '../lib/interfaces'
-// import fdbStore from '../lib/stores/fdb'
-// import runTests from './common'
-// import assert from 'assert'
+import 'mocha'
+import {I} from '@statecraft/core'
+import fdbStore from './fdb'
 
-// import * as fdb from 'foundationdb'
+// TODO: Its a little gross depending on the common tests like this. It would be
+// better to pull this suite out into yet another module.
+import runTests from '@statecraft/core/dist/test/common'
 
-// fdb.setAPIVersion(600)
+import * as fdb from 'foundationdb'
 
-// const TEST_PREFIX = '__test'
-// let _dbid = 1
-// const storeDb = new Map<I.Store<any>, fdb.Database>()
+fdb.setAPIVersion(600)
 
-// const create = async () => {
-//   const prefix = TEST_PREFIX + _dbid++
-//   const db = fdb.openSync().at(prefix)
-//   await db.clearRange('', Buffer.from([0xff])) // Hope there's no bugs in this one!
-//   const store = await fdbStore(db)
-//   // console.log('got store at prefix', prefix)
-//   storeDb.set(store, db)
-//   return store
-// }
+const TEST_PREFIX = '__test'
+let _dbid = 1
+const storeDb = new Map<I.Store<any>, fdb.Database>()
 
-// const teardown = (store: I.Store<any>) => { // teardown. Nuke it.
-//   const db = storeDb.get(store)!
-//   storeDb.delete(store)
-//   store.close()
-//   db.clearRange('', Buffer.from([0xff]))
-//   // TODO: And close the database.
-// }
+const create = async () => {
+  const prefix = TEST_PREFIX + _dbid++
+  const db = fdb.openSync().at(prefix)
+  await db.clearRange('', Buffer.from([0xff])) // Hope there's no bugs in this one!
+  const store = await fdbStore(db)
+  // console.log('got store at prefix', prefix)
+  storeDb.set(store, db)
+  return store
+}
 
-// describe('fdb', () => {
-//   try {
-//     const db = fdb.openSync()
-//     // db.close()
-//   } catch (e) {
-//     console.warn('Warning: Foundationdb instance not found. Skipping foundationdb tests. Error:', e.message)
-//     return
-//   }
+const teardown = (store: I.Store<any>) => { // teardown. Nuke it.
+  const db = storeDb.get(store)!
+  storeDb.delete(store)
+  store.close()
+  db.clearRange('', Buffer.from([0xff]))
+  // TODO: And close the database.
+}
 
-//   // it('hi', async () => {
-//   //   const store = await create()
-//   //   setTimeout(() => teardown(store), 10)
-//   // })
-//   runTests(create, teardown)
-// })
+describe('fdb', () => {
+  try {
+    const db = fdb.openSync()
+    // db.close()
+  } catch (e) {
+    console.warn('Warning: Foundationdb instance not found. Skipping foundationdb tests. Error:', e.message)
+    return
+  }
+
+  // it('hi', async () => {
+  //   const store = await create()
+  //   setTimeout(() => teardown(store), 10)
+  // })
+  runTests(create, teardown)
+})
